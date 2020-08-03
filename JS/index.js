61c90b3d711296dfd37aecca635a1542f4a57ac6
Toyesh Dash <tdashjitu@gmail.com>
2020-08-03 23:57:54 +0530

    Web project edit 1

---
+++ JS/index.js
@@ -0,0 +1,41 @@
+// console.log('hello world')
+//         // alert('me');
+// var num1 = 34;
+// var num2 = 56;
+// // console.log(num1+num2)
+
+// var marks = {
+//     a1: 56,
+//     a2: 36,
+//     Tdash: 90
+// }
+// // console.log(marks);
+
+// function  avg(a,b){
+//     return(a+b)/2
+// }
+// c = avg(4,5);
+// c2 = avg(234,41241);
+// console.log(c, c2)
+
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// // console.log(arr);
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//         continue;   
+//     }
+//     console.log(arr[i])
+// }
+
+// arr.forEach(function(i){
+//     console.log(i)
+// })
+
+// let j = 0;
+// while (j < arr.length) {
+//     console.log(arr[j]);
+//     j++
+// }
+let mydate = new Date();
+console.log(mydate);
+
